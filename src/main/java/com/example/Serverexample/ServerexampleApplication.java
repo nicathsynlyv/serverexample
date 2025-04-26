3f95845232488dce11c9fdd759fba8e61a20f17b
NicatHsynlyv <nicat200507@gmail.com>
2025-04-26 23:02:25 +0400

    servers

---
+++ src/main/java/com/example/Serverexample/ServerexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Serverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Serverexample/ServerexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Serverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
